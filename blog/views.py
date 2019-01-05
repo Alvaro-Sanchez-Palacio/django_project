9d88e0e66b788a54801828756988f5a27fbcc877
Álvaro Sánchez Palacio <alvaro.sanchez.palacio@gmail.com>
2019-01-05 19:10:58 +0100

    Add context variable to blog

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'Álvaro Sánchez',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 1, 2019',
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 2, 2019',
+    }
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
