b69a208231909e32cc86cfbce2ce2304b1033ab1
Álvaro Sánchez Palacio <alvaro.sanchez.palacio@gmail.com>
2019-01-07 10:37:35 +0100

    Added Database queries to Blog

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Álvaro Sánchez',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 1, 2019',
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 2, 2019',
-    }
-]
+from .models import Post
 
 
 def home(request):
+
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
